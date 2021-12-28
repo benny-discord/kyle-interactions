8d0533ef45a396840a8e453d0671b198d3d6c020
Skye <46286597+Skye-31@users.noreply.github.com>
2021-12-28 00:00:11 -0500

    first command

--- src/bot.js
+++ src/bot.js
@@ -11,6 +11,46 @@ export async function handleRequest(request) {
 		type: InteractionResponseType.Pong
 	})
 
+	if (interaction.type === InteractionType.ApplicationCommand) {
+		switch (interaction.data.name) {
+			case 'translation-status':
+				const json = await fetch('https://api.benny.sh/translations/info').then(r => r.json()).catch(() => null);
+				if (!json || json.status !== 200) return respond({
+					type: InteractionResponseType.ChannelMessageWithSource,
+					data: {
+						flags: 1 << 6,
+						content: 'Translation failed to fetch',
+					}
+				});
+
+				const { data } = json,
+					total = data.en;
+				delete data.en;
+
+				const embed = {
+					title: `Translation Status - ${total} total`,
+					description: Object.keys(data).sort().map(k => `**${k.toUpperCase()}**: ${data[k]}/${total} (${Math.round(data[k] / total * 1000) / 10}%)${data[k] === total ? ' <:verified:703946320670621716>' : ''}`).join('\n'),
+					color: 0x77fc8f
+				};
+				return respond({
+					type: InteractionResponseType.ChannelMessageWithSource,
+					data: {
+						flags: 1 << 6,
+						embeds: [embed]
+					}
+				});
+
+			default:
+				return respond({
+					type: InteractionResponseType.ChannelMessageWithSource,
+					data: {
+						flags: 1 << 6,
+						content: 'Unknown command',
+					}
+				});
+		}
+	}
+
 	return respond({
 		type: InteractionResponseType.ChannelMessageWithSource,
 		data: {
